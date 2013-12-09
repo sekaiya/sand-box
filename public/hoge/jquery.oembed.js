f2b3ebe656898afed5ae6bc0d2213f8d5e383d3c
sekaiya <sekai.kobayashi@gmail.com>
2013-12-09 13:33:34 +0900

    first

---
+++ public/hoge/jquery.oembed.js
@@ -0,0 +1,366 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ja">
+<head>
+  <meta http-equiv="Content-Language" content="ja">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="Content-Script-Type" content="text/javascript; charset=UTF-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <meta name="generator" content="Hiki 1.0.0">
+  <title>Rubyist Magazine - るびま</title>
+  <link rel="stylesheet" type="text/css" href="theme/rubima/../hiki_base.css" media="all">
+  <link rel="stylesheet" type="text/css" href="theme/rubima/rubima.css" media="all">
+  <link rel="icon" type="image/png" href="/favicon.png">
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <style type="text/css"><!--
+    span.adminmenu {
+      display: none;
+    }
+    --></style>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+  <script type="text/javascript" src="/js/jquery.oembed.js"></script>
+</head>
+
+<body>
+<div class="whole-contents">
+  <div class="contents">
+    <a name="top"> </a>
+<div class="main">
+  <div class="adminmenu"><span class="adminmenu"><a href="./">るびま</a></span>&nbsp;
+<span class="adminmenu"><a href="./?c=index">ページ一覧</a></span>&nbsp;
+<span class="adminmenu"><a href="./?c=search">検索</a></span>&nbsp;
+<span class="adminmenu"><a href="./?c=recent">更新履歴</a></span>&nbsp;
+<span class="adminmenu"><a href="./?c=login;p=FrontPage">ログイン</a></span></div>
+  <h1 class="header">るびま</h1>
+  <div>  <div class="social-buttons" style="text-align:right">
+      <!-- hatena bookmark button -->
+      <a href="http://b.hatena.ne.jp/entry/magazine.rubyist.net/?FrontPage" class="hatena-bookmark-button" title="Add this entry to Hatena Bookmark"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="Add this entry to Hatena Bookmark" width="20" height="20" style="border: none;" /></a>
+      <script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8"></script>
+      <!-- like button -->
+      <iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fmagazine.rubyist.net%2F?FrontPage&amp;layout=button_count&amp;show_faces=true&amp;width=100&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
+      <!-- tweet button -->
+      <a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="">Tweet</a>
+      <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+
+    </div>
+</div>
+  <div><div class="day">
+  <h2><span class="date"><a name="l0"> </a></span><span class="title">Rubyist Magazine について</span></h2>
+  <div class="body">
+    <div class="section">
+      <p>『Rubyist Magazine』、略して『るびま』は、日本 Ruby の会の有志による Rubyist の Rubyist による、Rubyist とそうでない人のためのウェブ雑誌です。</p>
+    </div>
+  </div>
+</div>
+<div class="day">
+  <h2><span class="date"><a name="l1"> </a></span><span class="title">最新号</span></h2>
+  <div class="body">
+    <div class="section">
+      <ul>
+<li><a href="./?0044">0044号(発行: 2013 9/30)</a></li>
+</ul>
+    </div>
+  </div>
+</div>
+<div class="day">
+  <h2><span class="date"><a name="l2"> </a></span><span class="title">バックナンバー</span></h2>
+  <div class="body">
+    <div class="section">
+      <ul>
+<li><a href="./?0043">0043号(発行: 2013 7/31)</a></li>
+<li><a href="./?0042">0042号(発行: 2013 5/29)</a></li>
+<li><a href="./?Ruby200SpecialEn">Ruby 2.0.0 Release special articles in English (2013 3/15)</a></li>
+<li><a href="./?0041">0041号(発行: 2013 2/24)</a></li>
+<li><a href="./?0040">0040号(発行: 2012 11/25)</a></li>
+<li><a href="./?0039">0039号(発行: 2012 9/5)</a></li>
+<li><a href="./?AnnounceRubyKaja">RubyKajaのご紹介(発行: 2012 6/17)</a></li>
+<li><a href="./?0038">0038号(発行: 2012 5/22)</a></li>
+<li><a href="./?0037">0037号(発行: 2012 2/5)</a></li>
+<li><a href="./?0036">0036号(発行: 2011 11/28)</a></li>
+<li><a href="./?0035">0035号(発行: 2011 9/26)</a></li>
+<li><a href="./?preRubyKaigi2011">日本 Ruby 会議 2011 直前特集号(発行: 2011 6/16)</a></li>
+<li><a href="./?0034">0034号(発行: 2011 6/12)</a></li>
+<li><a href="./?0033">0033号(発行: 2011 4/5)</a></li>
+<li><a href="./?0032">0032号(発行: 2011 1/31)</a></li>
+<li><a href="./?0031">0031号(発行: 2010 10/7)</a></li>
+<li><a href="./?preRubyKaigi2010">日本 Ruby 会議 2010 直前特集号(発行: 2010 7/26)</a></li>
+<li><a href="./?0030">0030号(発行: 2010 6/15)</a></li>
+<li><a href="./?0029">0029号(発行: 2010 3/16)</a></li>
+<li><a href="./?0028">0028号(発行: 2009 12/7)</a></li>
+<li><a href="./?0027">0027号(発行: 2009 9/13)</a></li>
+<li><a href="./?0026">0026号(発行: 2009 6/30)</a></li>
+<li><a href="./?0025">0025号(発行: 2009 2/7)</a></li>
+<li><a href="./?0024">0024号(発行: 2008 10/1)</a></li>
+<li><a href="./?0023">0023号(発行: 2008 3/31)</a></li>
+<li><a href="./?0022">0022号(発行: 2007 12/17)</a></li>
+<li><a href="./?0021">0021号(発行: 2007 9/29)</a></li>
+<li><a href="./?0020">0020号(発行: 2007 8/15)</a></li>
+<li><a href="./?0019">0019号(発行: 2007 5/18)</a></li>
+<li><a href="./?0018">0018号(発行: 2007 2/28)</a></li>
+<li><a href="./?0017">0017号(発行: 2006 11/26)</a></li>
+<li><a href="./?0016">0016号(発行: 2006 9/20)</a></li>
+<li><a href="./?0015">0015号(発行: 2006 7/13)</a></li>
+<li><a href="./?RubyKaigi2006">RubyKaigi2006特別号(発行: 2006 6/28)</a></li>
+<li><a href="./?0014">0014号(発行: 2006 5/15)</a></li>
+<li><a href="./?0013">0013号(発行: 2006 2/20)</a></li>
+<li><a href="./?0012">0012号(発行: 2005 12/23)</a></li>
+<li><a href="./?0011">0011号(発行: 2005 11/16)</a></li>
+<li><a href="./?0010">0010号(発行: 2005 10/10)</a></li>
+<li><a href="./?0009">0009号(発行: 2005 9/6)</a></li>
+<li><a href="./?0008">0008号(発行: 2005 7/19)</a></li>
+<li><a href="./?0007">0007号(発行: 2005 6/19)</a></li>
+<li><a href="./?0006">0006号(発行: 2005 5/9)</a></li>
+<li><a href="./?0005">0005号(発行: 2005 2/15)</a></li>
+<li><a href="./?0004">0004号(発行: 2004 12/17)</a></li>
+<li><a href="./?0003">0003号(発行: 2004 11/15)</a></li>
+<li><a href="./?0002">0002号(発行: 2004 10/16)</a></li>
+<li><a href="./?0001">創刊号(発行: 2004 9/10)</a></li>
+</ul>
+<ul>
+<li><a href="./?VolumeIndices">各号目次</a></li>
+<li><a href="./?CategoryIndices">分野別目次</a></li>
+</ul>
+    </div>
+  </div>
+</div>
+<div class="day">
+  <h2><span class="date"><a name="l3"> </a></span><span class="title">FAQ</span></h2>
+  <div class="body">
+    <div class="section">
+      <h3><a name="l4"><span class="sanchor"> </span></a>一般</h3>
+<ul>
+<li>Q. るびま、って「ネギま！」のぱくりですか？</li>
+<li>A. 違います。多分。「るびま」を考えた人たちは「ネギま！」を知りませんでした (または、言われるまで気付かなかった)。</li>
+</ul>
+<ul>
+<li>Q. 日本 Ruby の会って、るびまを発行することを目的とする団体ですか？</li>
+<li>A. 違います。るびまは、日本 Ruby の会の活動の一環として日本 Ruby の会有志によって編集作業が行われています。</li>
+</ul>
+<ul>
+<li>Q. 日本 Ruby の会発足とるびま創刊号発行は同時なの？（CNET の記事より）</li>
+<li>A. 違います。発足は 2004 8/8, 創刊号の発行は 2004 9/10 です。</li>
+</ul>
+<ul>
+<li>Q. 何が嬉しくてやってんの？</li>
+<li>A. 執筆者、編集者によります。営利目的じゃありません。</li>
+</ul>
+<ul>
+<li>Q. 月刊なんですか？</li>
+<li>A. 違います。</li>
+</ul>
+<ul>
+<li>Q. じゃぁ、何時出すんですか？</li>
+<li>A. 出せそうだと思ったら出します。</li>
+</ul>
+<ul>
+<li>Q. るびまに宣伝を出したいんだけれど。</li>
+<li>A. 可能です。詳しくは <a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> までご連絡ください。</li>
+</ul>
+<ul>
+<li>Q. るびまの読者プレゼントに提供したいものがあるんだけれど。</li>
+<li>A. 歓迎します。<a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> までご連絡ください。</li>
+</ul>
+<ul>
+<li>Q. 問い合わせはどこにすればいいの？</li>
+<li>A. <a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> にお願いします。</li>
+</ul>
+<h3><a name="l5"><span class="sanchor"> </span></a>記事</h3>
+<ul>
+<li>Q. るびまに〜〜の記事がないのは何故？　やる気あんの？</li>
+<li>A. 基本的に書いてくれる人任せです。書いてくれる人を探すことはしますが、無報酬でどこまで引き受けてくれるのか、という問題があります。あなたの投稿をお待ちしております。</li>
+</ul>
+<ul>
+<li>Q. こんな記事が読みたいんだけど。</li>
+<li>A. るびまの<a href="https://github.com/rubima/rubima-support" class="external">サポート用リポジトリ</a>があるので、そちらへ要望 Issue をどうぞ！　または、<a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> までご連絡ください。</li>
+</ul>
+<ul>
+<li>Q. こんな記事を書いてみたいんだけど。</li>
+<li>A. 待ってました！　ぜひ <a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> までご連絡ください。</li>
+</ul>
+<ul>
+<li>Q. 記事を書いたら何か報酬は出る？</li>
+<li>A. （少なくとも現状では）金銭的な報酬は一切出ません。ご了承ください。</li>
+</ul>
+<ul>
+<li>Q. 記事のライセンスってどうなっているんですか？</li>
+<li>A. 日本の国内法に従います。詳細に関しては <a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> までお問い合わせください。</li>
+</ul>
+<ul>
+<li>Q. 難易度の基準は？</li>
+<li>A. そのときの気分です。</li>
+</ul>
+<ul>
+<li>Q. 間違いや誤字を見つけたんだけど。</li>
+<li>A. るびまの<a href="https://github.com/rubima/rubima-support" class="external">サポート用リポジトリ</a>で Issue を作成してご指摘くださるか、<a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> までご連絡ください。おって訂正いたします。</li>
+</ul>
+<h3><a name="l6"><span class="sanchor"> </span></a>編集者のお仕事</h3>
+<ul>
+<li>Q. 編集を手伝いたいんですが。</li>
+<li>A. ありがとうございます。<a href="mailto:magazine@ruby-no-kai.org" class="external">るびま編集部</a> までご連絡ください。</li>
+</ul>
+<ul>
+<li>Q. 編集者って足りてないんですか？</li>
+<li>A. 足りてません。</li>
+</ul>
+<ul>
+<li>Q. 編集者って何するんですか？</li>
+<li>A. 主に、各種チェックを行ないます。<ul>
+<li>執筆者と連絡をとり、進捗をチェックします。これを、記事提出まで行ないます。</li>
+<li>記事の内容のチェックをします。記事の目的と合っているか、わかりづらい表現などはないか、などを見ます。</li>
+<li>記事の技術的内容をチェックします。サンプルコードの動作確認なども行ないます。</li>
+<li>記事の体裁をチェックします。</li>
+<li>記事の誤字脱字などをチェックします。</li>
+</ul></li>
+</ul>
+<ul>
+<li>Q. 本当に上記チェックを全部やってるの？</li>
+<li>A. やりたいんだけど人手が足りないので完璧にはできていません。やれる限りやっています。</li>
+</ul>
+<ul>
+<li>Q. 記事の英語へ翻訳、英語から翻訳などもやるの？</li>
+<li>A. そのような計画があります。</li>
+</ul>
+<ul>
+<li>Q. 企画を提案してもいい？</li>
+<li>A. 歓迎します。</li>
+</ul>
+<ul>
+<li>Q. 編集者になると、上記すべてをやらなければならないの？</li>
+<li>A. そんなことはありません。どれか一個でもやってもらえれば助かります。そもそも、「お願い」しかできませんから。</li>
+</ul>
+<ul>
+<li>Q. 編集者になると報酬をもらえますか？</li>
+<li>A. （少なくとも現状では）金銭的な報酬は一切出ません。ご了承ください。</li>
+</ul>
+    </div>
+  </div>
+</div>
+</div>
+  <div class="day">
+    <div class="comment">
+      <div class="caption">
+        更新日時:2013/09/30 23:04:46<br>
+        キーワード:<br>
+        参照:<br>
+        
+      </div>
+    </div>
+    <div>  <script type="text/javascript">
+          $(document).ready(function() {
+                  $("a.oembed").oembed(null, {
+                          maxWidth: 640,
+                          maxHeight: 480
+                  });
+          });
+  </script>
+</div>
+  </div>
+</div>
+
+<hr style="display: none">
+<div class="sidebar">
+  <form action="./" method="get">
+  <input type="text" name="key" size="15">
+  <input type="submit" name="comment" value="検索">
+  <input type="hidden" name="c" value="search">
+</form>
+
+<h4><a name="s0"> </a><a href="./?0044">0044号 (2013-09)</a></h4>
+<ul>
+<li><a href="./?0044-ForeWord">巻頭言</a></li>
+<li><a href="./?FirstStepRuby">Rubyの歩き方</a></li>
+<li><a href="./?0044-Hotlinks">Rubyist Hotlinks 大場さん</a></li>
+<li><a href="./?0044-Comments">九周年へのたより</a><ul>
+<li><a href="./?0044-EditorComment">九周年</a></li>
+</ul></li>
+<li><a href="./?0044-FluentdV11NewFeatures">Fluentd v11 の噂</a></li>
+<li><a href="./?0044-CRubyCommittersWhosWho2013">Committers Who's Who</a></li>
+<li><a href="./?0044-GuestTalk">他言語からの訪問 Perl</a></li>
+<li><a href="./?0044-TokyuRubyKaigi06Report">Regional Tokyu 06</a></li>
+<li><a href="./?0044-OkayamaRubyKaigi02Report">Regional 岡山 02</a></li>
+<li><a href="https://github.com/ruby-no-kai/official/wiki/RubyEventCheck" class="external">RubyEventCheck</a></li>
+<li><a href="./?0044-EditorsNote">編集後記</a></li>
+</ul>
+<h4><a name="s1"> </a>バックナンバー</h4>
+<ul>
+<li><a href="./?VolumeIndices">各号目次</a></li>
+<li><a href="./?CategoryIndices">分野別目次</a></li>
+<li><a href="./?0043">0043号 (2013-07)</a></li>
+<li><a href="./?0042">0042号 (2013-05)</a></li>
+<li><a href="./?Ruby200SpecialEn">2.0.0 Special (En)</a></li>
+<li><a href="./?0041">0041号 (2013-02)</a></li>
+<li><a href="./?0040">0040号 (2012-11)</a></li>
+<li><a href="./?0039">0039号 (2012-09)</a></li>
+<li><a href="./?AnnounceRubyKaja">RubyKajaのご紹介</a></li>
+<li><a href="./?0038">0038号 (2012-05)</a></li>
+<li><a href="./?0037">0037号 (2012-02)</a></li>
+<li><a href="./?0036">0036号 (2011-11)</a></li>
+<li><a href="./?0035">0035号 (2011-09)</a></li>
+<li><a href="./?preRubyKaigi2011">RubyKaigi2011直前特集号</a></li>
+<li><a href="./?0034">0034号 (2011-06)</a></li>
+<li><a href="./?0033">0033号 (2011-04)</a></li>
+<li><a href="./?0032">0032号 (2011-01)</a></li>
+<li><a href="./?0031">0031号 (2010-10)</a></li>
+<li><a href="./?preRubyKaigi2010">RubyKaigi2010直前特集号</a></li>
+<li><a href="./?0030">0030号 (2010-06)</a></li>
+<li><a href="./?0029">0029号 (2010-03)</a></li>
+<li><a href="./?0028">0028号 (2009-12)</a></li>
+<li><a href="./?0027">0027号 (2009-09)</a></li>
+<li><a href="./?0026">0026号 (2009-06)</a></li>
+<li><a href="./?0025">0025号 (2009-02)</a></li>
+<li><a href="./?0024">0024号 (2008-10)</a></li>
+<li><a href="./?0023">0023号 (2008-03)</a></li>
+<li><a href="./?0022">0022号 (2007-12)</a></li>
+<li><a href="./?0021">0021号 (2007-09)</a></li>
+<li><a href="./?0020">0020号 (2007-08)</a></li>
+<li><a href="./?0019">0019号 (2007-05)</a></li>
+<li><a href="./?0018">0018号 (2007-02)</a></li>
+<li><a href="./?0017">0017号 (2006-11)</a></li>
+<li><a href="./?0016">0016号 (2006-09)</a></li>
+<li><a href="./?0015">0015号 (2006-07)</a></li>
+<li><a href="./?RubyKaigi2006">RubyKaigi2006特別号</a></li>
+<li><a href="./?0014">0014号 (2006-05)</a></li>
+<li><a href="./?0013">0013号 (2006-02)</a></li>
+<li><a href="./?0012">0012号 (2005-12)</a></li>
+<li><a href="./?0011">0011号 (2005-11)</a></li>
+<li><a href="./?0010">0010号 (2005-10)</a></li>
+<li><a href="./?0009">0009号 (2005-09)</a></li>
+<li><a href="./?0008">0008号 (2005-07)</a></li>
+<li><a href="./?0007">0007号 (2005-06)</a></li>
+<li><a href="./?0006">0006号 (2005-05)</a></li>
+<li><a href="./?0005">0005号 (2005-02)</a></li>
+<li><a href="./?0004">0004号 (2004-12)</a></li>
+<li><a href="./?0003">0003号 (2004-11)</a></li>
+<li><a href="./?0002">0002号 (2004-10)</a></li>
+<li><a href="./?0001">0001号 (2004-09)</a></li>
+</ul>
+<h4><a name="s2"> </a>リンク</h4>
+<ul>
+<li><a href="http://www.ruby-lang.org/" class="external">オブジェクト指向言語Ruby</a></li>
+<li><a href="http://jp.rubyist.net/" class="external">日本Rubyの会</a></li>
+</ul>
+<p><a href="./"><img class="rubima_logo" src="theme/rubima/rubima_logo.png" alt="るびま" width="135" height="82"></a>
+<a href="http://jp.rubyist.net/"><img class="rubynokai" src="http://jp.rubyist.net/rubynokai01.png" alt="日本 Ruby の会" width="88" height="31"></a></p>
+
+</div>
+
+
+  </div>
+  <div class="footer">Generated by <a href="http://hikiwiki.org/">Hiki</a> 1.0.0 (2013-03-30).<br>
+Powered by <a href="http://www.ruby-lang.org/">Ruby</a> 2.0.0-p247 (2013-06-27).<br>
+Founded by RubiMa Editors.<br>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41117431-1', 'rubyist.net');
+  ga('send', 'pageview');
+
+</script>
+</div>
+</div>
+</body>
+</html>
